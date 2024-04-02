fbe3e86b5ba008c390c0da28a40f5b8014e6acf6
gabriela-vitor <gabriela.vitor2004@gmail.com>
2024-04-02 10:32:29 -0300

    add atividade2

--- atividade2/verificar_numero_primo.c
+++ atividade2/verificar_numero_primo.c
@@ -6,7 +6,32 @@ int main(int argc, char* argv[]){
 
     int num = atoi(argv[1]);
 
-    //se o numero for menor ou igual a um imprimir false, pois nenhum numero menor ou igual a um ehprimo
+if(num <= 1){
+    printf("false");
+    exit(0);
+}
+
+if( (num != 2) && (num % 2 == 0) ){
+    printf("false");
+    exit(0);
+}
+
+for(int i = 3; i < num ; i += 2){
+    if(num % i == 0){
+        printf("false");
+        exit(0);
+    }
+}
+
+printf("true");
+
+exit(0);
+}
+
+//ou
+
+
+ /*//se o numero for menor ou igual a um imprimir false, pois nenhum numero menor ou igual a um ehprimo
    if(num <= 1){
     printf("False\n");
     return 0;
@@ -28,3 +53,7 @@ int main(int argc, char* argv[]){
    printf("True\n");
    exit(0);
 }
+*/
+
+
+
