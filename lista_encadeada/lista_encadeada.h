68c2b9132b961127f401c7f701367353156a10bc
gabriela-vitor <gabriela.vitor2004@gmail.com>
2024-04-16 11:22:06 -0300

    add lista_encadeada

---
+++ lista_encadeada/lista_encadeada.h
@@ -0,0 +1,14 @@
+//guarda um valor e aponta para o proximo No
+typedef struct No{
+    char valor;
+    struct No* proximo_no;
+}No;
+
+// declara a funcao
+No* no(char valor, No* proximo_no);
+
+void inserir_no(No* H, No* no);
+void imprimir_lista(No* H);
+int quantidade_nos(No* H);
+No* copiar_lista(No* H);
+void liberar_lista(No* H);
